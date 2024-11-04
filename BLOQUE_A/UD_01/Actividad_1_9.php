6a653c48a1f0c321b07899aa1f7789fb169bdad8
Raúl García Alcántara <146923749+raulGarAlc04@users.noreply.github.com>
2024-11-04 17:16:54 +0100

    Noveno ejercicio

---
+++ BLOQUE_A/UD_01/Actividad_1_9.php
@@ -0,0 +1,18 @@
+<?php
+    $prefix = "Thank you";
+    $name = "Raul García";
+    $message = $prefix . ", " . $name;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad 1.9 Bloque A Raúl García</title>
+</head>
+<body>
+    <h1>The Candy Store</h1>
+    <h2><?= $name ?>'s Order</h2>
+    <p><?= $message ?></p>
+</body>
+</html>
