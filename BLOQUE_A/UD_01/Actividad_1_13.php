c73f630433f4e2193c97084bf30a7f385584dab5
Raúl García Alcántara <146923749+raulGarAlc04@users.noreply.github.com>
2024-11-05 09:58:25 +0100

    Actividad 13 cambios

--- BLOQUE_A/UD_01/Actividad_1_13.php
+++ BLOQUE_A/UD_01/Actividad_1_13.php
@@ -7,9 +7,26 @@
         "price" => 20,
         "discount" => 2,
     ];
+    $primeros = [
+        "Macarrones",
+        "Pez espada",
+        "Ensalada Mixta",
+    ];
+
+    $segundos = [
+        "Solomillo",
+        "Calamares",
+        "Paella",
+    ];
+
+    $postres = [
+        "Tarta",
+        "Profiteroles",
+        "Fruta variada",
+    ];
     $usual_price = $offer["qty"] * $offer["price"];
     $offer_price = $offer["qty"] * ($offer["price"] / $offer["discount"]);
-    $saving = $usual_price - $offer_price;
+    $saving = $usual_price - $offer_price; 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,8 +38,27 @@
 </head>
 <body>
     <h1>Restaurante Etcheverry</h1>
-    <h2>Oferta de menu 2x1</h2>
     <p><?= $greeting ?></p>
+    <h2>Oferta de menu 2x1</h2>
+    <h3>Platos incluidos en el menu</h3>
+    <h4>Primeros</h4>
+    <ul>
+        <li><?= $primeros[0] ?></li>
+        <li><?= $primeros[1] ?></li>
+        <li><?= $primeros[2] ?></li>
+    </ul>
+    <h4>Segundos</h4>
+    <ul>
+        <li><?= $segundos[0] ?></li>
+        <li><?= $segundos[1] ?></li>
+        <li><?= $segundos[2] ?></li>
+    </ul>
+    <h4>Postres</h4>
+    <ul>
+        <li><?= $postres[0] ?></li>
+        <li><?= $postres[1] ?></li>
+        <li><?= $postres[2] ?></li>
+    </ul>
     <p class = "sticker">Ahorras $<?= $saving ?></p>
     <p>Pide <?= $offer["qty"] ?> menus <?= $offer["item"] ?> por $<?= $offer_price ?><br>(Precio normal $<?= $usual_price ?>)</p>
 </body>
